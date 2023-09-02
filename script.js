1043307aa1949e02f30919920d4d6723c3ec1e98
GalasMichal <michalgalas1989@gmail.com>
2023-09-02 08:44:14 +0200

    Created Json

---
+++ script.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        'question': 'Wie heißt die Hauptstadt der Slowakei?',
+        'answer_1': 'Sofia',
+        'answer_2': 'Prag',
+        'answer_3': 'Bratislava',
+        'answer_4': 'Ljubljan',
+        'right_answer': 3,
+    }
+
+    {
+        'question': 'Wie viele Zähne hat ein erwachsener Mensch normalerweise?',
+        'answer_1': '26',
+        'answer_2': '30',
+        'answer_3': '32',
+        'answer_4': '36',
+        'right_answer': 3,
+    }
+
+    {
+        'question': 'Welches Land ist kein ständiges Mitglied im Sicherheitsrat der Vereinten Nationen?',
+        'answer_1': 'USA',
+        'answer_2': 'Russland',
+        'answer_3': 'Deutschland',
+        'answer_4': 'China',
+        'right_answer': 3,
+    }
+
+    {
+        'question': 'In welcher Einheit wird der elektrische Widerstand gemessen?',
+        'answer_1': 'Ohm',
+        'answer_2': 'Volt',
+        'answer_3': 'Ampere',
+        'answer_4': 'Watt',
+        'right_answer': 1,
+    }
+
+    {
+        'question': 'Wie lautet das chemische Symbol für Blei?',
+        'answer_1': 'Bl',
+        'answer_2': 'Pb',
+        'answer_3': 'Be',
+        'answer_4': 'Pt',
+        'right_answer': 2,
+    }
+]
